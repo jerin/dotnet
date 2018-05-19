72a39db4043860b96a1b930be830b0891ce01d60
jerin john <john.jerin2@gmail.com>
2018-05-19 11:42:32 +0530

    initial files

---
+++ WebAPIAuthentication/AuthContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIAuthentication.Entities;
+
+namespace WebAPIAuthentication
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
